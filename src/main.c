4fb0185c332a9a48a5cba362a641b2e9bfb803b5
Zealotree <zealotree@gmail.com>
2016-08-11 15:44:25 -0400

    Touch ups; added shake to disable seconds

--- src/main.c
+++ src/main.c
@@ -2,30 +2,17 @@
 Window *my_window;
 static Layer *main_clock;
 static Layer *clock_hands;
-static GPath *hour_arrow;
 static GTextAttributes *s_attributes;
 static char date_buffer[] = "31";
 
-
-static const GPathInfo HOUR_HAND_POINTS = {
-  9, (GPoint []){
-    { -2, 0 },
-	{ 2, 0 },
-	{ 2, -20 },
-	{ 4, -20 },
-    { 4, -59 },
-	{ 0, -63 },
-	{ -4, -59 },
-	{ -4, -20 },
-	{ -2, -20 }
-  }
-};
-
 // Layout boundaries
-#define TIME_RING PBL_IF_ROUND_ELSE(23, 20)
-#define DOT_RING PBL_IF_ROUND_ELSE(52, 20)
-#define DATE_RING PBL_IF_ROUND_ELSE(75, 20)
+#define TIME_RING PBL_IF_ROUND_ELSE(26, 20)
+#define DOT_RING PBL_IF_ROUND_ELSE(6, 20)
+#define DATE_RING PBL_IF_ROUND_ELSE(65, 20)
 
+
+// Persistent Keys
+#define SHOW_SECONDS 33
 
 // Bitmaps for Hours
 // H0 is 12 and 00
@@ -41,36 +28,36 @@ static int32_t get_angle_for_hour(int hour) {
 
 
 GBitmap* getBitmapFromHour(int num){
-	switch(num){
-	case 0:
-		return H0;
-	case 1:
-		return H1;
-	case 2:
-		return H2;
-	case 3:
-		return H3;
-	case 4:
-		return H4;
-	case 5:
-		return H5;
-	case 6:
-		return H6;
-	case 7:
-		return H7;
-	case 8:
-		return H8;
-	case 9:
-		return H9;
-	case 10:
-		return H10;
-	case 11:
-		return H11;
-	case 12:
-		return H0;
-	default:
-		return NULL;
-	}
+  switch(num){
+  case 0:
+    return H0;
+  case 1:
+    return H1;
+  case 2:
+    return H2;
+  case 3:
+    return H3;
+  case 4:
+    return H4;
+  case 5:
+    return H5;
+  case 6:
+    return H6;
+  case 7:
+    return H7;
+  case 8:
+    return H8;
+  case 9:
+    return H9;
+  case 10:
+    return H10;
+  case 11:
+    return H11;
+  case 12:
+    return H0;
+  default:
+    return NULL;
+  }
 }
 
 static int32_t get_angle_for_minute(int hour) {
@@ -84,42 +71,40 @@ static void draw_main_clock(Layer *layer, GContext *ctx) {
   GRect time_ring = grect_inset(bounds, GEdgeInsets(TIME_RING));
   GRect dot_ring = grect_inset(bounds, GEdgeInsets(DOT_RING));
   
-  	GRect frame = grect_inset(bounds, GEdgeInsets(2));
-	GRect inner_hour_frame = grect_inset(bounds, GEdgeInsets((4 * 1) + 2));
-GRect inner_minute_frame = grect_inset(bounds, GEdgeInsets((4 * 1) + 2));
   
-	// Minute Marks
-	graphics_context_set_stroke_color(ctx, GColorBlack);
-	graphics_context_set_stroke_width(ctx, 1);
-	for(int i = 0; i < 60; i++) {
-		if (i % 5) {
-			int minute_angle = get_angle_for_minute(i);
-			GPoint p0 = gpoint_from_polar(frame, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(minute_angle));
-			GPoint p1 = gpoint_from_polar(inner_minute_frame, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(minute_angle));
-			graphics_draw_line(ctx, p0, p1);
-		}
-	}
+  // Minute Marks
+  GRect frame = grect_inset(bounds, GEdgeInsets(2));
+  GRect inner_minute_frame = grect_inset(bounds, GEdgeInsets((4 * 1) + 2));
+  
+  // Minute Marks
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+  graphics_context_set_stroke_width(ctx, 2);
+  for(int i = 0; i < 60; i++) {
+    if (i % 5) {
+      int minute_angle = get_angle_for_minute(i);
+      GPoint p0 = gpoint_from_polar(frame, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(minute_angle));
+      GPoint p1 = gpoint_from_polar(inner_minute_frame, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(minute_angle));
+      graphics_draw_line(ctx, p0, p1);
+    }
+}
 
 
   
   // Draw Hour Markers
   for(int i = 0; i < 13; i++) {
+    GRect frame = grect_inset(bounds, GEdgeInsets(2));
+    GRect inner_hour_frame = grect_inset(bounds, GEdgeInsets((4 * 1) + 2));
+
     graphics_context_set_compositing_mode(ctx, GCompOpSet);
     graphics_context_set_fill_color(ctx, GColorBlack);
     GRect digits = grect_centered_from_polar(time_ring, GOvalScaleModeFitCircle, 
                                              DEG_TO_TRIGANGLE(get_angle_for_hour(i)), GSize(33,33));
     graphics_draw_bitmap_in_rect(ctx, getBitmapFromHour(i), digits);
-    
-        graphics_context_set_fill_color(ctx, GColorBlack);
-    GPoint tick = gpoint_from_polar(dot_ring, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(get_angle_for_hour(i)));
+
+    graphics_context_set_fill_color(ctx, GColorBlack);
+    GPoint tick = gpoint_from_polar(inner_hour_frame, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(get_angle_for_hour(i)));
     graphics_fill_circle(ctx, tick, 2);
-    
-    graphics_context_set_stroke_color(ctx, GColorBlack);
-    graphics_context_set_stroke_width(ctx, 1);
-    int hour_angle = get_angle_for_hour(i);
-    GPoint p0 = gpoint_from_polar(frame, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(hour_angle));
-		GPoint p1 = gpoint_from_polar(inner_hour_frame, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(hour_angle));
-		graphics_draw_line(ctx, p0, p1);
+
   }
   
 
@@ -137,30 +122,9 @@ static void draw_clock_hands(Layer *layer, GContext *ctx) {
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
   
-
-
-  // Draw Minute Hand
-  int minute_angle = TRIG_MAX_ANGLE * tick_time->tm_min / 60;
+  // Draw Second
   
-  int minute_hand_length = bounds.size.w / 2 - 5;
-
-  GPoint minute_hand = {
-    .x = (int16_t)(sin_lookup(minute_angle) * (int32_t)  minute_hand_length / TRIG_MAX_RATIO) + center.x,
-    .y = (int16_t)(-cos_lookup(minute_angle) * (int32_t) minute_hand_length / TRIG_MAX_RATIO) + center.y,
-  };
-  
-  graphics_context_set_stroke_width(ctx, 4);
-  graphics_context_set_stroke_color(ctx, GColorBlack);
-  graphics_draw_line(ctx, center, minute_hand);
-
-  
-	graphics_context_set_fill_color(ctx, GColorBlack);
-	gpath_rotate_to(hour_arrow, (TRIG_MAX_ANGLE * (((tick_time->tm_hour % 12) * 6) + (tick_time->tm_min / 10))) / (12 * 6));
-  gpath_draw_filled(ctx, hour_arrow);
-  
-    // Draw Second
-  
-  if (tick_time->tm_hour >= 23 || tick_time->tm_hour <= 5) {
+  if (persist_exists(SHOW_SECONDS) && persist_read_bool(SHOW_SECONDS)) {
     // Do not draw seconds from 23:00 to 5:00
   } else {
     int second_angle = TRIG_MAX_ANGLE * tick_time->tm_sec / 60;
@@ -187,26 +151,64 @@ static void draw_clock_hands(Layer *layer, GContext *ctx) {
     graphics_draw_line(ctx, second_hand_tail, center);
   }
 
+  // Draw Minute Hand
+  int minute_angle = TRIG_MAX_ANGLE * tick_time->tm_min / 60;
+  
+  int minute_hand_length = bounds.size.w / 2 - 5;
+
+  GPoint minute_hand = {
+    .x = (int16_t)(sin_lookup(minute_angle) * (int32_t)  minute_hand_length / TRIG_MAX_RATIO) + center.x,
+    .y = (int16_t)(-cos_lookup(minute_angle) * (int32_t) minute_hand_length / TRIG_MAX_RATIO) + center.y,
+  };
+  
+  graphics_context_set_stroke_width(ctx, 4);
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+  graphics_draw_line(ctx, center, minute_hand);
+  
+  int hour_angle = TRIG_MAX_ANGLE * (tick_time->tm_hour * 5 + tick_time->tm_min / 12) / 60;
+  int hour_hand_length = bounds.size.w / 3 - 10;
+
+  GPoint hour_hand = {
+    .x = (int16_t)(sin_lookup(hour_angle) * (int32_t)  hour_hand_length / TRIG_MAX_RATIO) + center.x,
+    .y = (int16_t)(-cos_lookup(hour_angle) * (int32_t) hour_hand_length / TRIG_MAX_RATIO) + center.y,
+  };
+  
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_context_set_stroke_width(ctx, 7);
+  graphics_draw_line(ctx, center, hour_hand);
+  
+
+
   
 
   // Draw Center Dot
-  graphics_fill_circle(ctx, center, 5);
+  graphics_fill_circle(ctx, center, 7);
   
   // Draw Current Date
   strftime(date_buffer, sizeof(date_buffer), "%d", tick_time);  
     GRect date = grect_centered_from_polar(date_ring, GOvalScaleModeFitCircle, 
-                                             DEG_TO_TRIGANGLE(get_angle_for_hour(6)), GSize(25,25));
+                                             DEG_TO_TRIGANGLE(get_angle_for_hour(6)), GSize(30,30));
   graphics_context_set_text_color(ctx, GColorBlack);
-  graphics_draw_text(ctx, date_buffer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD), date,
+  graphics_draw_text(ctx, date_buffer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD), date,
                 GTextOverflowModeWordWrap, GTextAlignmentCenter, s_attributes);
   
   
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits changed) {
-  if (SECOND_UNIT & changed) {
-      layer_mark_dirty(clock_hands);
+  layer_mark_dirty(clock_hands);
+}
+
+static void accel_tap_handler(AccelAxisType axis, int32_t direction) {
+  // A tap event occured
+  if (persist_exists(SHOW_SECONDS) && persist_read_bool(SHOW_SECONDS)) {
+    persist_write_bool(SHOW_SECONDS, false);
+  } else if (persist_exists(SHOW_SECONDS) && ! persist_read_bool(SHOW_SECONDS)) {
+    persist_write_bool(SHOW_SECONDS, true);
+  } else {
+        persist_write_bool(SHOW_SECONDS, true);
   }
+  layer_mark_dirty(clock_hands);
 }
 
 static void main_window_load() {
@@ -216,19 +218,17 @@ static void main_window_load() {
 
   Layer *window_layer = window_get_root_layer(my_window);
   GRect bounds = layer_get_bounds(window_layer);
-  GPoint center = grect_center_point(&bounds);
 
   main_clock = layer_create(bounds);
   clock_hands = layer_create(bounds);
   
-  hour_arrow = gpath_create(&HOUR_HAND_POINTS);
-  gpath_move_to(hour_arrow, center);
+
   
-	H0 = gbitmap_create_with_resource(RESOURCE_ID_RES_12); H1 = gbitmap_create_with_resource(RESOURCE_ID_RES_1);
-	H2 = gbitmap_create_with_resource(RESOURCE_ID_RES_2); H3 = gbitmap_create_with_resource(RESOURCE_ID_RES_3);
-	H4 = gbitmap_create_with_resource(RESOURCE_ID_RES_4); H5 = gbitmap_create_with_resource(RESOURCE_ID_RES_5);
-	H6 = gbitmap_create_with_resource(RESOURCE_ID_RES_6); H7 = gbitmap_create_with_resource(RESOURCE_ID_RES_7);
-	H8 = gbitmap_create_with_resource(RESOURCE_ID_RES_8); H9 = gbitmap_create_with_resource(RESOURCE_ID_RES_9);
+  H0 = gbitmap_create_with_resource(RESOURCE_ID_RES_12); H1 = gbitmap_create_with_resource(RESOURCE_ID_RES_1);
+  H2 = gbitmap_create_with_resource(RESOURCE_ID_RES_2); H3 = gbitmap_create_with_resource(RESOURCE_ID_RES_3);
+  H4 = gbitmap_create_with_resource(RESOURCE_ID_RES_4); H5 = gbitmap_create_with_resource(RESOURCE_ID_RES_5);
+  H6 = gbitmap_create_with_resource(RESOURCE_ID_RES_6); H7 = gbitmap_create_with_resource(RESOURCE_ID_RES_7);
+  H8 = gbitmap_create_with_resource(RESOURCE_ID_RES_8); H9 = gbitmap_create_with_resource(RESOURCE_ID_RES_9);
   H10 = gbitmap_create_with_resource(RESOURCE_ID_RES_10); H11 = gbitmap_create_with_resource(RESOURCE_ID_RES_11);
 
   
@@ -242,6 +242,7 @@ static void main_window_load() {
   layer_add_child(window_layer, clock_hands);
 
   tick_handler(tick_time, MINUTE_UNIT);
+  accel_tap_service_subscribe(accel_tap_handler);
 }
 
 
@@ -260,7 +261,7 @@ static void main_window_unload() {
   gbitmap_destroy(H9);
   gbitmap_destroy(H10);
   gbitmap_destroy(H11);
-  gpath_destroy(hour_arrow);
+  accel_tap_service_unsubscribe();
 }
 
 static void app_connection_handler(bool connected) {
